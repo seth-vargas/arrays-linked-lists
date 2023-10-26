a7e665d69b9cd28d6aebf60de0b33f22e99e0a03
Seth Vargas <shvargas01@gmail.com>
2023-10-26 01:58:47 -0500

    Initial commit

---
+++ linked-list.js
@@ -0,0 +1,22 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+  constructor(vals = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (let val of vals) this.push(val);
+  }
+}
+
+module.exports = LinkedList;
